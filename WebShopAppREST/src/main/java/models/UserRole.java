736fb33c72fad42453d9a39ae035bc8dbd17fd76
lostojicc <ostojiclukaftn@gmail.com>
2024-05-16 02:10:30 +0200

    feat: Added user and customer models
    - wow

---
+++ WebShopAppREST/src/main/java/models/UserRole.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum UserRole {
+	Customer,
+	Worker,
+	Manager,
+	Administrator
+}
---
+++ WebShopAppREST/src/main/java/utils/DateUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+	private static final ThreadLocal<SimpleDateFormat> DATE_FORMATTER = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd/MM/yyyy"));
+
+    public static Date parse(String dateString) throws ParseException {
+        return DATE_FORMATTER.get().parse(dateString);
+    }
+
+    public static String format(Date date) {
+        return DATE_FORMATTER.get().format(date);
+    }
+}

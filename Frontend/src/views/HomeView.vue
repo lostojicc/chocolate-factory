97d39f2ecbfef0881e605f7ccc25f969cbe1b70a
lostojicc <ostojiclukaftn@gmail.com>
2024-05-23 22:11:25 +0200

    feat: Started working on login form

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
+<template>
+    <h1>adsaidjsio</h1>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>

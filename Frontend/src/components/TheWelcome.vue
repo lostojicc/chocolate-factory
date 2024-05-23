959ef092325a5952e0349d259215ba51c633da8a
djukic002 <strahinjadjukic95@gmail.com>
2024-05-23 17:56:36 +0200

    feat: Added frontend base

---
+++ Frontend/src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="welcome">
+    <h2>Welcome to your Vue 3 project!</h2>
+
+    <div>
+      <RouterLink to="/login">Login</RouterLink>
+    </div>
+  </section>
+</template>

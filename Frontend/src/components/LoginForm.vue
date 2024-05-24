b306b7d43c2d947c1204038f57dc185f6b46e1e5
lostojicc <ostojiclukaftn@gmail.com>
2024-05-24 18:51:27 +0200

    feat: Login form looks done

--- Frontend/src/components/LoginForm.vue
+++ Frontend/src/components/LoginForm.vue
@@ -3,84 +3,56 @@
             <div class="container">
                 <div class="row g-0">
                     <div class="col-1">
-                        <!-- <img src="img/background-site.jpg" class="img-fluid h-100 w-100 rounded-start" style="object-fit: cover; opacity: 0.7;" alt=""> -->
+                        <img src="../img/background-site.jpg" class="img-fluid h-100 w-100 rounded-start" style="object-fit: cover; opacity: 0.7;" alt="">
                     </div>
                     <div class="col-10">
                         <div class="border-bottom border-top border-primary bg-light py-5 px-4">
                             <div class="text-center">
-                                <small class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Book Us</small>
-                                <h1 class="display-5 mb-5">Where you want Our Services</h1>
+                                <small class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">Welcome</small>
+                                <h1 class="display-5 mb-5">Login using your credentials</h1>
                             </div>
                             <div class="row g-4 form">
-                                <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>Select Country</option>
-                                        <option value="1">USA</option>
-                                        <option value="2">UK</option>
-                                        <option value="3">India</option>
-                                    </select>
+                                <div class="col-lg-3 col-md-6">
+    
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                
+                                        <input type="text" class="form-control border-primary p-2" placeholder="Enter Your Username">
+                                    
+                                </div>
+                                <div class="col-lg-3 col-md-6">
+
+                                </div>
+                                <div class="col-lg-3 col-md-6">
+
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <input type="text" class="form-control border-primary p-2" placeholder="Enter Your Password">
+                                </div>
+                                <div class="col-lg-3 col-md-6">
+
                                 </div>
                                 <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>Select City</option>
-                                        <option value="1">Depend On Country</option>
-                                        <option value="2">UK</option>
-                                        <option value="3">India</option>
-                                    </select>
+
                                 </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>Select Palace</option>
-                                        <option value="1">Depend On Country</option>
-                                        <option value="2">UK</option>
-                                        <option value="3">India</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>Small Event</option>
-                                        <option value="1">Event Type</option>
-                                        <option value="2">Big Event</option>
-                                        <option value="3">Small Event</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>No. Of Palace</option>
-                                        <option value="1">100-200</option>
-                                        <option value="2">300-400</option>
-                                        <option value="3">500-600</option>
-                                        <option value="4">700-800</option>
-                                        <option value="5">900-1000</option>
-                                        <option value="6">1000+</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <select class="form-select border-primary p-2" aria-label="Default select example">
-                                        <option selected>Vegetarian</option>
-                                        <option value="1">Vegetarian</option>
-                                        <option value="2">Non Vegetarian</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <input type="mobile" class="form-control border-primary p-2" placeholder="Your Contact No.">
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <input type="date" class="form-control border-primary p-2" placeholder="Select Date">
-                                </div>
-                                <div class="col-lg-4 col-md-6">
-                                    <input type="email" class="form-control border-primary p-2" placeholder="Enter Your Email">
-                                </div>
-                                <div class="col-12 text-center">
+                                <div class="col-4 text-center stackpanel">
                                     <button type="submit" class="btn btn-primary px-5 py-3 rounded-pill">Submit Now</button>
+                                    <label>Don't have an account? <router-link :to="'/register'">Register now</router-link></label>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-1">
-                        <!-- <img src="img/background-site.jpg" class="img-fluid h-100 w-100 rounded-end" style="object-fit: cover; opacity: 0.7;" alt=""> -->
+                        <img src="../img/background-site.jpg" class="img-fluid h-100 w-100 rounded-end" style="object-fit: cover; opacity: 0.7;" alt="">
                     </div>
                 </div>
             </div>
         </div>
 </template>
+
+<style scoped>
+    .stackpanel{
+        display: flex;
+        flex-direction: column;
+    }
+</style>

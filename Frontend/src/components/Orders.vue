47ad2cefad11a64f595561b425116f09f324e8c0
djukic002 <strahinjadjukic95@gmail.com>
2024-06-30 15:16:29 +0200

    feat: Shopping and order completed
    -Added sorting and search

--- Frontend/src/components/Orders.vue
+++ Frontend/src/components/Orders.vue
@@ -13,18 +13,20 @@
                                 <label class="text-dark mx-4">
                                     Factory:
                                 </label>
-                                <input type="text" class="form-control border-primary p-2 textWidth mx-2" placeholder="Enter Factory"/>
+                                <input type="text" class="form-control border-primary p-2 textWidth mx-2" placeholder="Enter Factory" v-model="search.factoryName"/>
                             </div>
                             <div class="col-4 d-flex align-items-center justify-content-center">
                                 <label class="text-dark mx-4">
                                     Price:
                                 </label>
                                 <div class="d-flex justify-content-start align-items-center">
-                                    <input type="number" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Price"/>
+                                    <input type="number" class="form-control border-primary p-2 numberWidth mx-2" min="1" max="150000" step="0.01"
+                                     placeholder="Price" v-model="search.minPrice"/>
                                     <label class="text-dark">
                                         -
                                     </label>
-                                    <input type="number" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Price"/>
+                                    <input type="number" class="form-control border-primary p-2 numberWidth mx-2" min="1" max="150000" step="0.01"
+                                    placeholder="Price" v-model="search.maxPrice"/>
                                 </div>
                             </div>
                             <div class="col-4 d-flex align-items-center justify-content-center">
@@ -32,11 +34,11 @@
                                     Date:
                                 </label>
                                 <div class="d-flex justify-content-start align-items-center">
-                                    <input type="date" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Price"/>
+                                    <input type="date" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Min Price" v-model="search.minDate"/>
                                     <label class="text-dark">
                                         -
                                     </label>
-                                    <input type="date" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Price"/>
+                                    <input type="date" class="form-control border-primary p-2 numberWidth mx-2" placeholder="Max Price" v-model="search.maxDate"/>
                                 </div>
                             </div>
                             <div class="d-flex align-items-center justify-content-center">
@@ -55,37 +57,37 @@
                     <div class="row d-flex justify-content-center px-3 py-3">
                         <div class="d-flex justify-content-around align-items-center">
                             <div v-if="userRole==='Customer'" class="col-4 d-flex justify-content-center align-items-center">
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortNameDown()">
                                     <i class="fas fa-sort-down"></i>
                                 </a>
                                 <label class="text-dark border-bottom border-primary">
                                     Factory Name
                                 </label>
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortNameUp()">
                                     <i class="fas fa-sort-up"></i>
                                 </a>
                             </div>
 
                             <div class="col-4 d-flex justify-content-center align-items-center mx-2">
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortPriceDown()">
                                     <i class="fas fa-sort-down"></i>
                                 </a>
                                 <label class="text-dark border-bottom border-primary">
                                     Price
                                 </label>
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortPriceUp()">
                                     <i class="fas fa-sort-up"></i>
                                 </a>
                             </div>
 
                             <div class="col-4 d-flex justify-content-center align-items-center">
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortDateDown()">
                                     <i class="fas fa-sort-down"></i>
                                 </a>
                                 <label class="text-dark border-bottom border-primary">
                                     Date
                                 </label>
-                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="UpdateUsername()">
+                                <a class="btn btn-primary btn-sm-square m-2 rounded-circle d-flex  align-items-center justify-content-center" @click="SortDateUp()">
                                     <i class="fas fa-sort-up"></i>
                                 </a>
                             </div>
@@ -119,6 +121,16 @@ const userRole = localStorage.getItem('role') || '';
 const username = localStorage.getItem('username') || '';
 const router = useRouter();
 
+const search = ref({
+    factoryName: '',
+    minPrice: '',
+    maxPrice: '',
+    minDate: '',
+    maxDate: ''
+});
+
+const searchActive = ref(false)
+
 const orders = ref([])
 
 onMounted(async () => {
@@ -126,11 +138,18 @@ onMounted(async () => {
 });
 
 function load(){
-    if(userRole === 'Customer'){
+    if(userRole === 'Customer' && !searchActive.value){
         loadCustomer()
     }
-    if(userRole === 'Manager'){
+    if(userRole === 'Manager' && !searchActive.value){
         loadManager()
+    }
+    if(userRole === 'Customer' && searchActive.value){
+        searchCustomer()
+    }
+    if(userRole === 'Manager' && searchActive.value){
+        searchManager()
+        search.value.factoryName = ''
     }
 }
 
@@ -166,6 +185,87 @@ function loadCustomer(){
     });
 }
 
+function searchCustomer(){
+    axios.post(`http://localhost:8080/WebShopAppREST/rest/order/searchForUser/${username}`, search.value ,{
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+        }
+    }).then(response => {
+        if (response.status === 200) {
+            orders.value = response.data
+        }
+    }).catch(error => {
+        console.error(error.response.data + " | Error status: " + error.response.status);
+        alert('Error loading orders!')
+        router.push('/')
+    });
+}
+
+function searchManager(){
+    axios.post(`http://localhost:8080/WebShopAppREST/rest/order/searchForManager/${username}`, search.value ,{
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+        }
+    }).then(response => {
+        if (response.status === 200) {
+            orders.value = response.data
+        }
+    }).catch(error => {
+        console.error(error.response.data + " | Error status: " + error.response.status);
+        alert('Error loading orders!')
+        router.push('/')
+    });
+}
+
+function Search(){
+    searchActive.value = true;
+    load();
+}
+
+function ClearSearch(){
+    search.value.factoryName = ''
+    search.value.minDate = ''
+    search.value.maxDate = ''
+    search.value.minPrice = ''
+    search.value.maxPrice = ''
+
+    searchActive.value = false;
+    load();
+}
+
+function SortNameUp() {
+    orders.value.sort((a, b) => {
+      return a.factoryName.localeCompare(b.factoryName);
+  });
+}
+
+function SortNameDown(){
+    orders.value.sort((a, b) => {
+      return b.factoryName.localeCompare(a.factoryName);
+  });
+}
+
+function SortPriceUp() {
+    orders.value.sort((a, b) => a.price - b.price);
+}
+
+function SortPriceDown(){
+    orders.value.sort((a, b) => b.price - a.price);
+}
+
+function SortDateUp() {
+    orders.value.sort((a, b) => 
+        parseDate(a.dateTime) - parseDate(b.dateTime));
+}
+
+function SortDateDown(){
+    orders.value.sort((a, b) => parseDate(b.dateTime) - parseDate(a.dateTime));
+}
+
+function parseDate(dateString) {
+  const cleanDateString = dateString.replace('[UTC]', '');
+  return new Date(cleanDateString);
+}
 
 </script>
 
